c76b464ec14a41872b0f92036521684804cdc5cf
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 18:26:49 +0200

    Add Animal and attributes

---
+++ EcoSim/Assets/Model/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+public class Animal
+{
+    //SECTION: Attributes and properties
+    private AnimalState _state;
+    public AnimalState State { get => _state; set => _state = value; }
+    //END: Attributes and properties
+
+    //SECTION: Constructor and main methods
+    //END: Constructor and main methods
+}
