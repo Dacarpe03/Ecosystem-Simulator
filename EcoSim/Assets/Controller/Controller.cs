e7b7c5a2064cad6fbe2751654068796372a0e0d6
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 23:19:24 +0200

    Add View attribute to controller

--- EcoSim/Assets/Controller/Controller.cs
+++ EcoSim/Assets/Controller/Controller.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    private Ecosystem ecosystem;
+    private Ecosystem _ecosystem;
+    private View _myView;
     // Start is called before the first frame update
     void Start()
     {
-        this.ecosystem = new Ecosystem();
+        this._ecosystem = new Ecosystem();
+        this._myView = Instantiate(_myView);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.ecosystem.Update();
+        this._ecosystem.Update();
     }
 }
--- EcoSim/Assets/View/View.cs
+++ EcoSim/Assets/View/View.cs
@@ -6,10 +6,4 @@ public class View : MonoBehaviour
 {
     public AnimalGroupView _group1;
     public AnimalGroupView _group2;
-
-    public View()
-    {
-        this._group1 = Instantiate(_group1);
-        this._group2 = Instantiate(_group2);
-    }
 }
