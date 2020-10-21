2a362346bf922733531539473bf5aad86799c2ba
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 18:34:33 +0200

    Add contructor and Transition methods

--- EcoSim/Assets/Model/Animal.cs
+++ EcoSim/Assets/Model/Animal.cs
@@ -9,5 +9,15 @@ public class Animal
     //END: Attributes and properties
 
     //SECTION: Constructor and main methods
+    public Animal(AnimalState state)
+    {
+        this.TransitionTo(state);
+    }
+
+    public void TransitionTo(AnimalState newState)
+    {
+        this._state = newState;
+        this._state.Eco = this;
+    }
     //END: Constructor and main methods
 }
