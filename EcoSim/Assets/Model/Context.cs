6f8d3c4097b5bb8a59008f7541a478351d919091
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 12:36:12 +0200

    Context attributes

---
+++ EcoSim/Assets/Model/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Context
+{
+    //SECTION: Attributes and properties
+    private int _iteration;
+    public int Iteration{
+        get
+        {
+            return iteration;
+        }
+    }
+
+    private AnimalGroup preys;
+    public AnimalGroup Preys
+    {
+        get
+        {
+            return preys;
+        }
+    }
+
+    private AnimalGroup predators; 
+    public
+    //END: Attributes and properties
+}
