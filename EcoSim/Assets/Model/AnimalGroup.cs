9bff779d2901c13adad643073c42e10d8d0037cb
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 14:42:01 +0200

    Add AnimalGroup class
    Atributes:
    * _size for the size of the group
    * _animals a list with all the members of the group
    * Animals: Only read property of _animals
    Methods:
    * Survive: to update the animal's position in the survive phase of the simulations
    * Evolve: so that the animals reproduce
    * GetPositions: to retrieve all the positions of the animals so that they can be put in the view

--- EcoSim/Assets/Model/AnimalGroup.cs
+++ EcoSim/Assets/Model/AnimalGroup.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public abstract class AnimalGroup
 {
     //SECTION: Attributes and properties
     private int _size;
+
+    private List<Animal> _animals;
+    private List<Animal> Animals { get => _animals; }
     //END: Attributes and properties
 
     //SECTION: Constructor and main methods
     public abstract void Update();
     public abstract void Evolve();
+
+    public List<Vector3> GetPositions()
+    {
+        List<Vector3> positions = new List<Vector3>();
+        foreach(Animal a in _animals)
+        {
+            positions.Add(a.Position);
+        }
+    }
     //END: Constructor and main methods
 }
