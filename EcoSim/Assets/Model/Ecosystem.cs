a4697f90bb9061cbf12a5ed1b31675cbff829be9
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 13:58:09 +0200

    Add transitionState method to Ecosystem
    Method to transition between simulation states in ecosystem

--- EcoSim/Assets/Model/Ecosystem.cs
+++ EcoSim/Assets/Model/Ecosystem.cs
@@ -24,15 +24,20 @@ public class Ecosystem
     //SECTION: Constructor and main methods
     public Ecosystem()
     {
-        State = new SimulationHuntState(this);
-        _iteration = 0;
-        _preys = new PreyGroup(PREY_GROUP_SIZE);
-        _predators = new PredatorGroup(PREDATOR_GROUP_SIZE);
+        this.State = new SimulationSurviveState(this);
+        this._iteration = 0;
+        this._preys = new PreyGroup(PREY_GROUP_SIZE);
+        this._predators = new PredatorGroup(PREDATOR_GROUP_SIZE);
     }
 
     public void Update()
     {
-        State.Update();
+        this.State.Update();
+    }
+
+    public void TransitionState(SimulationState newState)
+    {
+        this.State = newState;
     }
     //END: Constructor and main methods
 }
