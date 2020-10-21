20881fff85bc245fbc07c9561f627e7324967ffe
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 22:51:58 +0200

    Add View class

---
+++ EcoSim/Assets/View/View.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View : MonoBehaviour
+{
+    public AnimalGroupView _group1;
+    public AnimalGroupView _group2;
+
+    public View()
+    {
+        this._group1 = Instantiate(_group1);
+        this._group2 = Instantiate(_group2);
+    }
+}
