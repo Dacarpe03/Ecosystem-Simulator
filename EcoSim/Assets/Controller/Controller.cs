f8d86f14c7665a7f8f39ca9e27d2b5838cc11e37
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 22:37:35 +0200

    Add Controller Prefab

--- EcoSim/Assets/Controller/Controller.cs
+++ EcoSim/Assets/Controller/Controller.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,6 +15,7 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Console.WriteLine("Estoy funcionando");
         this.ecosystem.Update();
     }
 }
