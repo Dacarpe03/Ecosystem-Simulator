630650006c2f80a5fff80ceafcb2320d37a6f305
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-21 19:13:13 +0200

    Add constructors for Vec3

--- EcoSim/Assets/Model/Vec3.cs
+++ EcoSim/Assets/Model/Vec3.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
 
-public class Vec3 : MonoBehaviour
+public class Vec3
 {
+    //SECTION: Attributes and properties
     private float _xCoord;
     public float XCoord { get => _xCoord; }
 
@@ -12,5 +11,29 @@ public class Vec3 : MonoBehaviour
 
     private float _zCoord;
     public float ZCoord { get => _yCoord; }
+    //END: Attributes and properties
+
+
+    //SECTION: Constructor and main methods
+    public Vec3(float x, float y, float z)
+    {
+        this._xCoord = x;
+        this._yCoord = y;
+        this._zCoord = z;
+    }
+
+    public Vec3()
+    {
+        this.RandomizeCoords();
+    }
+
+    public void RandomizeCoords()
+    {
+        var rand = new Random();
+        this._xCoord = (float) rand.NextDouble() * 100;
+        this._yCoord = (float)rand.NextDouble() * 100;
+        this._zCoord = (float)rand.NextDouble() * 100;
+    }
+    //END: Constructor and main methods
 
 }
