e44f0d45da99a1b6ca24b9b45f7bf5b0c3ca3857
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-28 18:27:35 +0100

    GetSlowestAnimal method
    Self explanatory

--- EcoSim/Assets/Model/AnimalHuntState.cs
+++ EcoSim/Assets/Model/AnimalHuntState.cs
@@ -4,9 +4,31 @@ using System.Collections.Generic;
 public class AnimalHuntState : AnimalState
 {
     private Boolean preyFixed;
+    private int preyFixedId;
+
     public override void Update(List<Animal> friendly, List<Animal> foes)
     {
-        List<Animal> nearbyPreys = this.GetNearbyAnimals(foes, this._agent.SquaredVisionRadius);
+        if (!preyFixed) 
+        {
+            List<Animal> nearbyPreys = this.GetNearbyAnimals(foes, this._agent.SquaredVisionRadius);
+            preyFixedId = getSlowestAnimal(nearbyPreys);
+        }
     }
+    
+    private int getSlowestAnimal(List<Animal> nearbyPreys)
+    {
+        double maxSpeed = Double.MaxValue;
+        int idFixed = -1;
+        foreach(Animal p in nearbyPreys)
+        {
+            double preySpeed = p.Speed.SquaredModule;
+           if(preySpeed < maxSpeed)
+            {
+                maxSpeed = preySpeed;
+                idFixed = p.Id;
+            }
+        }
 
+        return idFixed;
+    }
 }
