af922aad2477b1598d70d5bef59505a46b32009a
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-28 18:08:57 +0100

    Start Update Method in this state

--- EcoSim/Assets/Model/AnimalHuntState.cs
+++ EcoSim/Assets/Model/AnimalHuntState.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class AnimalHuntState : AnimalState
 {
+    private Boolean preyFixed;
+    public override void Update(List<Animal> friendly, List<Animal> foes)
+    {
+        List<Animal> nearbyPreys = this.GetNearbyAnimals(foes, this._agent.SquaredVisionRadius);
+    }
 
+    private 
 }
