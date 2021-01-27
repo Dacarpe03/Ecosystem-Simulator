e34eac099a7ac9a1a5444153f2fcb5bde8869291
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-01-27 17:44:47 +0100

    Comment Controller

--- EcoSim/Assets/Controller/Controller.cs
+++ EcoSim/Assets/Controller/Controller.cs
@@ -44,40 +44,56 @@ public class Controller : MonoBehaviour
     void Start()
     {
         Debug.Log("Simulación " + this._simulationCounter);
+
+        //TODO: Create a class that contains all the initial paremeters of a group
+        //Initialize the ecosystem
         this._ecosystem = new Ecosystem(PREY_GROUP_SIZE, PREY_MAX_SPEED, PREY_VISION_RADIUS, PREY_REPRODUCTION_PROB, PREDATOR_GROUP_SIZE, PREDATOR_MAX_SPEED, PREDATOR_VISION_RADIUS, PREDATOR_REPRODUCTION_PROB);
+        
+        //Initialize the view
         this._myView = Instantiate(MyView);
         this._myView.Initialize(PREY_GROUP_SIZE, PREDATOR_GROUP_SIZE);
 
         this.PATH = Application.dataPath + "/SimulationData/";
         this.CreateFileForSimulation();
-    }
+    }//End Start
+
 
     // Update is called once per frame
     void Update()
     {
+        //If we haven't completed the number of iterations fixed or there is no animal group extinguised we keep on the loop
         if (this._ecosystem.Iteration < ITERATIONS_PER_SIMULATION & !this._ecosystem.Extinguised)
         {
-
+            //If the ecosystem has resseted (the iteration has finished), then we reset the view and save the data in the file
             if (this._ecosystem.Reset)
             {
                 this.UpdateFile();
                 this._ecosystem.Update();
                 this.ResetView();
             }
+            //If not we update the ecosystem (the next step/frame of the iteration)
             else
             {
+                //Update the model
                 this._ecosystem.Update();
+                
+                //Pass the information of positions from model to view
                 List<Vector3> preyModelPositions = this.GetModelPositions(this._ecosystem.Preys);
                 List<Vector3> predatorModelPositions = this.GetModelPositions(this._ecosystem.Predators);
                 this._myView.UpdatePositions(preyModelPositions, predatorModelPositions);
             }
         }
+        //If the simulation has finished, we start another one if necessary
         else if (this._simulationCounter < this.NUMBER_OF_SIMULATIONS)
         {
             this._simulationCounter++;
             Debug.Log("Simulación " + this._simulationCounter);
+
+            //Initialize the ecosystem
             this._ecosystem = new Ecosystem(PREY_GROUP_SIZE, PREY_MAX_SPEED, PREY_VISION_RADIUS, PREY_REPRODUCTION_PROB, PREDATOR_GROUP_SIZE, PREDATOR_MAX_SPEED, PREDATOR_VISION_RADIUS, PREDATOR_REPRODUCTION_PROB);
+            //Reset the view
             this.ResetView();
+            //Create a new file to save the data of the simulation
             this.CreateFileForSimulation();
         }
         else
@@ -85,16 +101,21 @@ public class Controller : MonoBehaviour
             Debug.Log("Fin de las simulaciones");
             Destroy(this.gameObject);
         }
-    }
+    }//End Update
 
+    
+    //TODO: Change this method so it is encapsulated, (for example return two lists of positions)
+    //Gets all the positions of an animal group
     public List<Vector3> GetModelPositions(AnimalGroup modelGroup)
     {
         List<Vec3> modelPositions = modelGroup.GetPositions();
         List<Vector3> viewPositions = this.TransformToVector3(modelPositions);
 
         return viewPositions;
-    }
+    }//End GetModelPositions
 
+    
+    //Transforms from Vec3 to Vector3
     public List<Vector3> TransformToVector3(List<Vec3> vectors)
     {
         List<Vector3> newVectors = new List<Vector3>();
@@ -105,22 +126,35 @@ public class Controller : MonoBehaviour
         }
 
         return newVectors;
-    }
+    }//End TransformToVector3
 
+
+    //Resets the view at the end of an iteration
     public void ResetView()
     {
+        //Destroys the current one
         this._myView.Reset();
         Destroy(this._myView.gameObject);
+
+        //Initializes another one
         this._myView = Instantiate(MyView);
         this._myView.Initialize(this._ecosystem.Preys.Size, this._ecosystem.Predators.Size);
-    }
+    }//End ResetView
 
+
+    //TODO: Save all the information of an iteration in the same line
+    //TODO: Change the separators to only one character
+    //TODO: Change the name of the file to stage_x_sim_y
+    //Creates a file to save the data from a simulation
     public void CreateFileForSimulation()
     {
+        //Create the name of the file
         var dt = DateTime.Now;
         string date = dt.ToString("MM-dd-yyyy-hh-mm");
         string fileName = date + "-simulation" + this._simulationCounter + ".txt";
         this._currentFileName = this.PATH + fileName;
+
+        //Write the first lines of the file
         if (!File.Exists(this._currentFileName))
         {
             StreamWriter sr = File.CreateText(_currentFileName);
@@ -137,8 +171,12 @@ public class Controller : MonoBehaviour
             sr.WriteLine("Iteracion|SupervivientesPresas|SupervivientesPredadores");
             sr.Close();
         }
-    }
+    }//End CreateFileForSimulation
 
+
+
+    //TODO: Save all the information in one line
+    //Saves the data from an iteration
     public void UpdateFile()
     {
         if (File.Exists(this._currentFileName))
@@ -148,7 +186,7 @@ public class Controller : MonoBehaviour
             sr.WriteLine(this._ecosystem.Iteration + "||" + this._ecosystem.Preys.SurvivorsNumber + "||" + this._ecosystem.Predators.SurvivorsNumber);
             sr.Close();
         }
-    }
+    }//End UpdateFile
 }
 
     
