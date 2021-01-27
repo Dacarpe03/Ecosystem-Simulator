19615694aea62ae25265b144280d61a78e898e8b
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-01-27 19:09:38 +0100

    Comment AnimalGroup

--- EcoSim/Assets/Model/AnimalGroup.cs
+++ EcoSim/Assets/Model/AnimalGroup.cs
@@ -21,7 +21,7 @@ public class AnimalGroup
 
 
     //SECTION: Constructor and main methods
-
+    //TODO: Use Builder pattern to create the different animal groups so delete de booolean prey parameter in builder
     public AnimalGroup(int size, double maxSpeed, double visionRadius, double reproductionProb, Boolean prey)
     {
         this._arePrey = prey;
@@ -43,23 +43,33 @@ public class AnimalGroup
         }
     }
 
+
+    //Method to update the position of animals in the iteration
     public void Survive(List<Animal> foes)
     {
+        //Get the animals from the same group that are not dead and are not safe
         List<Animal> aliveAllies = this._animals.Where(a => !a.IsDead | !a.IsSafe).Select(a => a).ToList();
-        List<Animal> aliveFoes = this._animals.Where(a => !a.IsDead | !a.IsSafe).Select(a => a).ToList();
+
+        //Get the animals from the other group that are not dead and are not safe
+        List<Animal> aliveFoes = foes.Where(a => !a.IsDead | !a.IsSafe).Select(a => a).ToList();
         foreach (Animal a in this._animals)
         {
             a.State.Update(aliveAllies, foes);
         }
-    }
+    }//End Survive
 
+
+    //Method to create the next generation of animals
     public void Evolve()
     {
         List<Animal> survivors = this._animals.Where(a => !a.IsDead & a.IsSafe).Select(a => a).ToList();
         //Debug.Log("MODELO--Tamaño grupo supervivientes: " + survivors.Count);
 
+        //Calculate the maximum breeding count
         int possibleBreedingCount = survivors.Count / 2;
         var rand = new System.Random();
+
+        //Now each pair tries to reproduce
         for (int i = 0; i < possibleBreedingCount; i++)
         {
             double r = rand.NextDouble();
@@ -72,18 +82,23 @@ public class AnimalGroup
 
         this._animals = survivors;
         //Debug.Log("Tamaño grupo después de reproducción: " + this.Size);
+        //Reset the animals in the model
         this.ResetSafe();
         this.ResetPositions();
-    }
+    }//END Evolve
 
+
+    //Reset the safe attribute to False
     public void ResetSafe()
     {
         foreach(Animal a in this._animals)
         {
             a.IsSafe = false;
         }
-    }
+    }//END ResetSafe
 
+
+    //Reset Positions
     public void ResetPositions()
     {
         System.Random rand = new System.Random();
@@ -94,11 +109,15 @@ public class AnimalGroup
             {
                 initialState = new AnimalFleeState();
             }
+
+            //Reset to the initial state
             a.TransitionTo(initialState);
+            //Reset the position in the initial square
             a.ResetPosition(rand);
         }
     }
 
+    //Returns the positions of all its animals
     public List<Vec3> GetPositions()
     {
         List<Vec3> positions = new List<Vec3>();
@@ -108,8 +127,10 @@ public class AnimalGroup
         }
 
         return positions;
-    }
+    }//END GetPositions
 
+
+    //Return true if all the animals are safe
     public Boolean AreSafe()
     {
         foreach(Animal a in this._animals)
