938aca99d8a4ada985a77a3f14b37ed42808efe2
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-01-27 18:14:37 +0100

    Comment SimulationSuviveState

--- EcoSim/Assets/Model/SimulationSurviveState.cs
+++ EcoSim/Assets/Model/SimulationSurviveState.cs
@@ -5,8 +5,10 @@ public class SimulationSurviveState : SimulationState
     //SECTION: Constructor and main methods
     public override void Update()
     {
+        //Is all the alive preys are safe, then the iteration is about to finish
         if (_eco.Preys.AreSafe())
         {
+            //Activate the 'reset condition' so that the view resets in the thex update call in Controller
             this._eco.Reset = true;
             this._eco.TransitionTo(new SimulationEvolveState());
         }
