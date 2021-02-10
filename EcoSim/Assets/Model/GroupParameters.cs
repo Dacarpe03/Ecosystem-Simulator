2388e89d20a450ddda725f9b05c55d3c4fa90c05
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-10 12:38:54 +0100

    Add attributes and parameters in GroupParameters

--- EcoSim/Assets/Model/GroupParameters.cs
+++ EcoSim/Assets/Model/GroupParameters.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class GroupParameters : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class GroupParameters
+{ 
 
-    // Update is called once per frame
-    void Update()
+    private double _reproductionProb;
+    public double ReproductionProb { get => _reproductionProb; }
+
+    private double _maxSpeed;
+    public double MaxSpeed { get => _maxSpeed; }
+
+    private int _groupSize;
+    public int GroupSize { get => _groupSize; }
+
+    public GroupParameters(double repProb, double maxSpeed, int size)
     {
-        
+        this._reproductionProb = repProb;
+        this._maxSpeed = maxSpeed;
+        this._groupSize = size;
     }
 }
