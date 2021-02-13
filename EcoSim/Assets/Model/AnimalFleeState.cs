d725a327448bced4580ab6fee3dbaf69240a0de0
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-13 22:27:39 +0100

    Delete Boids Parameters from AnimalFleeState

--- EcoSim/Assets/Model/AnimalFleeState.cs
+++ EcoSim/Assets/Model/AnimalFleeState.cs
@@ -4,12 +4,6 @@ using System.Collections.Generic;
 public class AnimalFleeState : AnimalState
 {
 
-    private const double WEIGHT_AVOID = 7;
-    private const double WEIGHT_COHESION = 2;
-    private const double WEIGHT_FOLLOW = 3;
-    private const double WEIGHT_CENTER = 10;
-    private const double WEIGHT_FLEE = 20;
-
     private Boid _boidBehaviour = new Boid();
 
     //Creates a force vector which is the result from Boids algorithm
