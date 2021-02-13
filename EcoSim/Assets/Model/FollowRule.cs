01fe418399ca1ea2289ff578e8be35d1b7379897
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-13 19:58:24 +0100

    Add FollowRule

---
+++ EcoSim/Assets/Model/FollowRule.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRule : BoidRule
+{
+    public FollowRule(double w) : base(w) { }
+
+    public override Vec3 CalculateForce(Animal agent, List<Animal> animals)
+    {
+        int animalCount = animals.Count;
+        if (animalCount > 0)
+        {
+            Vec3 meanSpeed = Vec3.Zero();
+            foreach (Animal a in animals)
+            {
+                meanSpeed.Add(a.Speed);
+            }
+
+            meanSpeed.Divide(animalCount);
+            meanSpeed.Multiply(this._weight);
+
+            return meanSpeed;
+        }
+        else
+        {
+            return Vec3.Zero();
+        }
+    }
+
+}
