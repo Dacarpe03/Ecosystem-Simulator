cac9fe056d2b1e826cc6311b4a59cfe81d2c8aaa
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-13 17:25:15 +0100

    Add Constructor to AnimalBuilder

--- EcoSim/Assets/Model/AnimalBuilder.cs
+++ EcoSim/Assets/Model/AnimalBuilder.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public abstract class AnimalBuilder
 {
-    protected GroupParameters animalParameters;
+    protected GroupParameters _animalParameters;
+
+
+    public AnimalBuilder(GroupParameters parameters)
+    {
+        this._animalParameters = parameters;
+    }
 
     public abstract Animal CreateAnimal();
     public abstract AnimalState GetAnimalState();
