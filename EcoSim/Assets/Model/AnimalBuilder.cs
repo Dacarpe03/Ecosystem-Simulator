a74041f86436ed7dbc2ffab98eb90ea90585b09d
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-13 17:41:46 +0100

    Add PreyBuilder

--- EcoSim/Assets/Model/AnimalBuilder.cs
+++ EcoSim/Assets/Model/AnimalBuilder.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public abstract class AnimalBuilder
 {
     protected GroupParameters _animalParameters;
-
+    protected int _creationCounter;
 
     public AnimalBuilder(GroupParameters parameters)
     {
         this._animalParameters = parameters;
+        this._creationCounter = 0;
     }
 
-    public abstract Animal CreateAnimal();
+    public abstract Animal CreateAnimal(Random rand);
     public abstract AnimalState GetAnimalState();
 }
