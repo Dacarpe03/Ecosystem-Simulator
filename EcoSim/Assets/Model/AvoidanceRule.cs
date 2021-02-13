9bfbc396e7edba56401174c1cc314d7946015aa6
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-13 19:37:05 +0100

    Create AvoidanceRule class

---
+++ EcoSim/Assets/Model/AvoidanceRule.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class AvoidanceRule : BoidRule
+{
+    public override Vec3 CalculateForce(Animal agent, List<Animal> animals)
+    {
+        throw new System.NotImplementedException();
+    }
+}
