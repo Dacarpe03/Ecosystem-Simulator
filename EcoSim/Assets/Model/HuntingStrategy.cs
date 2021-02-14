0fe239ec37a9ebecc9a4eccfe7e9780816f540d0
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-14 19:10:46 +0100

    Update Hunting Strategy

--- EcoSim/Assets/Model/HuntingStrategy.cs
+++ EcoSim/Assets/Model/HuntingStrategy.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
-public interface HuntingStrategy
+interface HuntingStrategy
 {
     public Vec3 Hunt(List<Animal> friendly, List<Animal> foes);
 }
