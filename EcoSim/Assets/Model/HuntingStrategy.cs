4df0c0aabbb445ca42a3e19476da0df4fa616ed5
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-14 19:06:34 +0100

    Create class Hunting Strategy

---
+++ EcoSim/Assets/Model/HuntingStrategy.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface HuntingStrategy
+{
+    public Vec3 Hunt(List<Animal> friendly, List<Animal> foes);
+}
