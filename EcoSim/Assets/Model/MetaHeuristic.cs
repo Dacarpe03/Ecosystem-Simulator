074ac218b816a38791865fa6bbb742e6b544f857
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-15 13:42:06 +0100

    Add Interface Metaheuristic

---
+++ EcoSim/Assets/Model/MetaHeuristic.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface MetaHeuristic
+{
+    double objectiveFunction(Dictionary<int, Animal> predators, Animal prey);
+}
