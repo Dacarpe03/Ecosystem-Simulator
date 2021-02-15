8a1a9556abd4078192d6a8cf5740a4c9bab55a8f
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-15 14:19:14 +0100

    Create GWOStrategy

---
+++ EcoSim/Assets/Model/GWOStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GWOStrategy : HuntingStrategy, MetaHeuristic
+{
+    public override void Hunt(Animal agent, Dictionary<int, Animal> friendly, Dictionary<int, Animal> foes)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public double objectiveFunction(Dictionary<int, Animal> predators, Animal prey)
+    {
+        throw new System.NotImplementedException();
+    }
+}
