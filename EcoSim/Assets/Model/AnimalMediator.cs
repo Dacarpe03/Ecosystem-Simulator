7f7a4e8a7951044035da7ab15087119adf58978d
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-15 12:27:19 +0100

    Create AnimalMediator class
    Used in some hunting strategies

---
+++ EcoSim/Assets/Model/AnimalMediator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class AnimalMediator
+{
+    private int _fixedPreyId = -1;
+    public int FixedPreyId { get => _fixedPreyId; set => this._fixedPreyId = value; }
+
+    private Dictionary<int, Animal> _predators;
+    public Dictionary<int, Animal> Predators { get => this._predators; }
+
+    public void AddAnimal(Animal a)
+    {
+        this._predators.Add(a.Id, a);
+    }
+
+
+}
