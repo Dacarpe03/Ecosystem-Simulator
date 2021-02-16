41ce1a08f96895a7fe46681a3e610ba7c2d7a48a
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-02-16 18:06:35 +0100

    Finish method GWO algorithm

--- EcoSim/Assets/Model/GWOStrategy.cs
+++ EcoSim/Assets/Model/GWOStrategy.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System;
 using System.Linq;
-
+using UnityEngine;
 public class GWOStrategy : HuntingStrategy, MetaHeuristic
 {
-    private int METAHEURISTIC_ITERATIONS = 100;
-    private int METAHEURISTIC_CANDIDATES = 5; //MUST BE >= 3
-    private int SIZE_OF_SPACE = 20;
+    private int METAHEURISTIC_ITERATIONS = 50;
+    private int METAHEURISTIC_CANDIDATES = 6; //MUST BE >= 3
+    private int SIZE_OF_SPACE = 10;
+    private Vec3 _desiredPosition = new Vec3(0, 0, 0);
+    private Boolean fixedPosition = false;
 
     private class CandidateSolution
     {
@@ -26,41 +28,56 @@ public class GWOStrategy : HuntingStrategy, MetaHeuristic
 
     public override void Hunt(Animal agent, Dictionary<int, Animal> friendly, Dictionary<int, Animal> foes)
     {
-        //TODO: Check if the prey is hunted before doing anything
         if(agent.AnimalMediator.FixedPreyId == -1)
         {
             agent.AnimalMediator.UpdateBestPreyId(foes);
+            this.fixedPosition = false;
         }
-
-        //Get all the positions
-        List<Vec3> predatorPositions = new List<Vec3>();
-
-        foreach (Animal a in friendly.Values)
+        else if(!this.fixedPosition || agent.Position.SquaredDistanceTo(this._desiredPosition) < 9)
         {
-            if (agent.Id == a.Id)
+
+            Debug.Log("Calculamos");
+            //Get all the positions
+            List<Vec3> predatorPositions = new List<Vec3>();
+
+            foreach (Animal a in friendly.Values)
             {
-                predatorPositions.Add(a.Position);
+                if (agent.Id == a.Id)
+                {
+                    predatorPositions.Add(a.Position);
+                }
             }
+
+            //Get data from fixedPrey
+            Animal fixedPrey = foes[agent.AnimalMediator.FixedPreyId];
+
+            //Calculate optimal position of the predator agent
+            this._desiredPosition= this.GreyWolfOptimizer(agent, predatorPositions, fixedPrey);
+
+            //Update the predator Speed
+            Vec3 acceleration = Vec3.CalculateVectorsBetweenPoints(agent.Position, this._desiredPosition);
+            acceleration.Expand(agent.MaxSpeed);
+            agent.UpdateSpeed(acceleration);
+            agent.Move();
+
+            this.fixedPosition = true;
+
+            Debug.Log("Lobo: " + agent.Id + " va a la posicion (" + this._desiredPosition.XCoord + "," + this._desiredPosition.ZCoord);
+        }
+        else
+        {
+            Vec3 acceleration = Vec3.CalculateVectorsBetweenPoints(agent.Position, this._desiredPosition);
+            acceleration.Expand(agent.MaxSpeed);
+            agent.UpdateSpeed(acceleration);
+            agent.Move();
         }
 
-        //Get data from fixedPrey
-        Animal fixedPrey = foes[agent.AnimalMediator.FixedPreyId];
-
-        //Calculate optimal position of the predator agent
-        Vec3 desiredAgentPosition = this.GreyWolfOptimizer(agent, predatorPositions, fixedPrey);
-        
-        //Update the predator Speed
-        Vec3 acceleration = Vec3.CalculateVectorsBetweenPoints(agent.Position, desiredAgentPosition);
-        acceleration.Expand(agent.MaxSpeed);
-        agent.UpdateSpeed(acceleration);
-        agent.Move();
     }
 
 
     public Vec3 GreyWolfOptimizer(Animal agent, List<Vec3> predatorPositions, Animal fixedPrey)
     {
-        //Initialize the candidateSolutions in the 
-        Random rand = new Random();
+        System.Random rand = new System.Random();
 
         //Declare the space of solutions
         double xUpperLimit = agent.Position.XCoord + this.SIZE_OF_SPACE;
@@ -83,25 +100,29 @@ public class GWOStrategy : HuntingStrategy, MetaHeuristic
         {
             //Order the candidates by fitness (the lower the better)
             candidates = candidates.OrderBy(c => c.Fitness).ToList();
-
             //Fittest Solutions
             Vec3 alphaSol = candidates[0].Solution.Clone();
             Vec3 betaSol = candidates[1].Solution.Clone();
             Vec3 gammaSol = candidates[2].Solution.Clone();
 
-            foreach(CandidateSolution cand in candidates)
+            Debug.Log("Mejor fitness iteracion" + i + "lobo "+ agent.Id +" : " + candidates[0].Fitness);
+            //Debug.Log("Segunda fitness iteracion" + i + "lobo " + agent.Id + " : " + candidates[1].Fitness);
+            //Debug.Log("Tercera fitness iteracion" + i + "lobo " + agent.Id + " : " + candidates[2].Fitness);
+            for (int j = 3; j < candidates.Count; j++)
             {
-                Vec3 X1 = CalculateVectorX(cand.Solution, alphaSol, rand, i);
-                Vec3 X2 = CalculateVectorX(cand.Solution, betaSol, rand, i);
-                Vec3 X3 = CalculateVectorX(cand.Solution, gammaSol, rand, i);
+                Vec3 omegaSolution = candidates[j].Solution;
+                Vec3 X1 = CalculateVectorX(omegaSolution, alphaSol, rand, i);
+                Vec3 X2 = CalculateVectorX(omegaSolution, betaSol, rand, i);
+                Vec3 X3 = CalculateVectorX(omegaSolution, gammaSol, rand, i);
 
                 //(X1+X2+X3)/3
                 X1.Add(X2);
                 X1.Add(X3);
                 X1.Divide(3);
 
-                cand.Solution = X1;
-                cand.Fitness = this.CalculateFitness(X1, predatorPositions, fixedPrey);
+                //Update the omegaSolution
+                candidates[j].Solution = X1;
+                candidates[j].Fitness = this.CalculateFitness(X1, predatorPositions, fixedPrey);
             }
         }
 
@@ -111,16 +132,17 @@ public class GWOStrategy : HuntingStrategy, MetaHeuristic
     }
 
     //To calculate X1, X2, X3 form the gwo equations
-    private Vec3 CalculateVectorX(Vec3 candidate, Vec3 optimalSolution, Random rand, int iteration)
+    private Vec3 CalculateVectorX(Vec3 candidate, Vec3 optimalSolution, System.Random rand, int iteration)
     {
         //Random vectors (coords between 0 and 1)
         Vec3 r1 = new Vec3(1, 0, 0, 0, 1, 0, rand);
 
         //C = 2r2, in this line we calculate r2 and C at the same time
-        Vec3 C = new Vec3(2, 0, 0, 0, 2, 0, rand);
+        Vec3 C = new Vec3(1, 0, 0, 0, 1, 0, rand);
+        C.Multiply(2);
 
-        //Scalar a (coords go from 2 to 0 lineally during the iterations)
-        double coord = 2 - iteration * (2 / this.METAHEURISTIC_ITERATIONS);
+        //Scalar a (coords go from 2 to 0 linearly during the iterations)
+        double coord = 2 - (iteration * (2 / this.METAHEURISTIC_ITERATIONS));
         Vec3 a = new Vec3(coord, 0, coord);
 
         //A vector A = 2*a*r1 - a
@@ -145,11 +167,11 @@ public class GWOStrategy : HuntingStrategy, MetaHeuristic
     {
         //Calculate where the prey would move if the agent moves to the possible position 
         Vec3 preyDirection = Vec3.CalculateVectorsBetweenPoints(candidatePosition, preyPosition);
-        double distance = preyDirection.Module;
+        //double distance = preyDirection.Module;
         preyDirection.Trim(maxSquaredPreySpeed);
 
-        double force = preyVisionRadius / distance;
-        preyDirection.Multiply(force);
+        //double force = preyVisionRadius / distance;
+        //preyDirection.Multiply(force);
 
         return Vec3.Add(preyDirection, preyPosition);
     }
@@ -158,6 +180,7 @@ public class GWOStrategy : HuntingStrategy, MetaHeuristic
     public double ObjectiveFunction(List<Vec3> predatorPositions, Vec3 preyPosition)
     {
         //We want to minimize the minimun distance of all the predators to the prey
+        double maxDistance = 0;
         double minDistance = double.MaxValue;
 
         //Calculate the distance
@@ -171,11 +194,18 @@ public class GWOStrategy : HuntingStrategy, MetaHeuristic
         }
 
         return minDistance;
+
     }
+
+    public double ObjFunc(Vec3 pos, Vec3 preyPosition)
+    {
+        return Vec3.CalculateVectorsBetweenPoints(pos, preyPosition).Module;
+    }
+
 
     private double CalculateFitness(Vec3 candidateSolution, List<Vec3> predatorPositions, Animal prey)
     {
         Vec3 predictedPreyPosition = this.PredictPreyPosition(candidateSolution, prey.Position, prey.MaxSquaredSpeed, prey.VisionRadius);
-        return ObjectiveFunction(predatorPositions, prey.Position);
+        return ObjFunc(candidateSolution, prey.Position);
     }
 }
