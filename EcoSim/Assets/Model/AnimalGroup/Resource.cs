cadc8094e7bc0040f848e555709223c53be29251
daniel.carmona <daniel.carmona@stormharvester.com>
2021-06-01 18:14:35 +0100

    Add Resource class

---
+++ EcoSim/Assets/Model/AnimalGroup/Resource.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Resource
+{
+    private double _quantity;
+    private double _increaseFactor;
+
+    public Resource (double initialQuantity, double increaseFactor)
+    {
+        this._quantity = initialQuantity;
+        this._increaseFactor = increaseFactor;
+    }
+
+    public bool resourcesAvailable()
+    {
+        if (this._quantity > 1)
+        {
+            this._quantity -= 1;
+            return true;
+        }
+        else
+        {
+            this._quantity = 0;
+            return false;
+        }
+    }
+
+    public void Increase()
+    {
+        this._quantity *= this._increaseFactor;
+    }
+}
