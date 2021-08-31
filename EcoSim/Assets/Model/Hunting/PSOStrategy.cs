aa2bc809b20e57da1f2a5c8307e7d6db085a2ea3
daniel.carmona <daniel.carmona@stormharvester.com>
2021-08-31 16:58:07 +0100

    Add PSO Strategy

---
+++ EcoSim/Assets/Model/Hunting/PSOStrategy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class PSOStrategy : HuntingStrategy, MetaHeuristic
+{
+    public override Vec3 GetDesiredPosition(Animal agent, List<Vec3> friendlyPositions, Animal prey)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override int GetFixedPreyId(Animal agent)
+    {
+        return agent.Mediator.FixedPreyId;
+    }
+
+    public override bool HasFixedPrey(Animal agent)
+    {
+        return agent.Mediator.FixedPreyId != -1;
+    }
+
+    public override void HuntPrey(Animal agent, Animal prey)
+    {
+        agent.Mediator.PreyHunted(prey);
+    }
+
+    public override void SelectPrey(Dictionary<int, Animal> friendly, Dictionary<int, Animal> foes, Animal agent)
+    {
+        agent.Mediator.UpdateBestPreyId(friendly, foes);
+    }
+
+    public double ObjectiveFunction(List<Vec3> predatorPositions, Vec3 preyPosition, Vec3 candidateSolution)
+    {
+        //We want to minimize the maximum distance of all the predators to the prey
+        double maxDistance = candidateSolution.SquaredDistanceTo(preyPosition);
+        //double maxDistance = 0;
+        //double minDistance = double.MaxValue;
+        //Calculate the distance
+        foreach (Vec3 position in predatorPositions)
+        {
+            double sqrdDistance = position.SquaredDistanceTo(preyPosition);
+            if (sqrdDistance > maxDistance)
+            {
+                maxDistance = sqrdDistance;
+            }
+        }
+
+        return maxDistance;
+    }
+}
