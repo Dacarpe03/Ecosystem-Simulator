d5ca2943ee7fafd3efcce37335a9de89d0f4c3bb
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-09-13 19:52:45 +0200

    WOA implemented, it is the worst

--- EcoSim/Assets/Model/Hunting/WOAStrategy.cs
+++ EcoSim/Assets/Model/Hunting/WOAStrategy.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class WOAStrategy : HuntingStrategy, MetaHeuristic
 {
-    private int METAHEURISTIC_ITERATIONS = 50;
-    private int METAHEURISTIC_CANDIDATES = 3;
-    private int SIZE_OF_SPACE = 50;
+    private int METAHEURISTIC_ITERATIONS = 300;
+    private int METAHEURISTIC_CANDIDATES = 25;
+    private int SIZE_OF_SPACE = 30;
+
+    private double b = 1;
 
     private class CandidateSolution
     {
@@ -57,12 +60,39 @@ public class WOAStrategy : HuntingStrategy, MetaHeuristic
             //Fittest Solutions
             Vec3 bestSolution = candidates[0].Solution.Clone();
 
-            //Debug.Log("Mejor fitness iteracion" + i + "lobo "+ agent.Id +" : " + candidates[0].Fitness);
-            //Debug.Log("Segunda fitness iteracion" + i + "lobo " + agent.Id + " : " + candidates[1].Fitness);
-            //Debug.Log("Tercera fitness iteracion" + i + "lobo " + agent.Id + " : " + candidates[2].Fitness);
-            for (int j = 0; j < candidates.Count; j++)
+            for (int j = 1; j < candidates.Count; j++)
             {
-                Vec3 A = CalculateAVector(i, rand);
+                Vec3 r = new Vec3(1, 0, 0, 0, 1, 0, rand);
+                Vec3 A = CalculateAVector(i, r);
+                Vec3 C = CalculateCVector(r);
+                double l = rand.NextDouble();
+
+                //Probability
+                double p = rand.NextDouble();
+
+                if (p <= 0.5)
+                {
+                    if (A.Module < 1) //Update the position of the current agent
+                    {
+                        Vec3 newSolution = EncirclePrey(A, C, bestSolution, candidates[j].Solution);
+                        candidates[j].Solution = newSolution;
+                        candidates[j].Fitness = CalculateFitness(newSolution, predatorPositions, fixedPrey);
+                    }
+                    else //Select random agent and update the position of current agent based on that
+                    {
+                        int randomAgent = rand.Next(0, candidates.Count);
+                        Vec3 newSolution = ExplorationPhase(A, C, candidates[randomAgent].Solution, candidates[j].Solution);
+                        candidates[j].Solution = newSolution;
+                        candidates[j].Fitness = CalculateFitness(newSolution, predatorPositions, fixedPrey);
+                    }
+                }
+                else //Spiral updating positions
+                {
+                    if (p < 0.75) { l *= -1; }
+                    Vec3 newSolution = SpiralUpdate(this.b, l, bestSolution, candidates[j].Solution);
+                    candidates[j].Solution = newSolution;
+                    candidates[j].Fitness = CalculateFitness(newSolution, predatorPositions, fixedPrey);
+                }
             }
         }
 
@@ -72,21 +102,66 @@ public class WOAStrategy : HuntingStrategy, MetaHeuristic
         return candidates[0].Solution;
     }
 
-    private Vec3 CalculateAVector(int iteration, System.Random rand)
+    private Vec3 CalculateAVector(int iteration, Vec3 r)
     {
         //Scalar a (coords go from 2 to 0 linearly during the iterations)
         double coord = 2 - (iteration * (2 / this.METAHEURISTIC_ITERATIONS));
         Vec3 a = new Vec3(coord, 0, coord);
 
         //Random vector (coords between 0 and 1)
-        Vec3 r = new Vec3(1, 0, 0, 0, 1, 0, rand);
 
-        //A vector
+        //A vector = 2a*r - a
         Vec3 A = Vec3.WolfProduct(a, r);
         A.Multiply(2);
         A.Substract(a);
 
         return A;
+    }
+    private Vec3 CalculateCVector(Vec3 r)
+    {
+        //C = 2*r
+        Vec3 C = r.Clone();
+        C.Multiply(2);
+
+        return C;
+    }
+
+    private Vec3 EncirclePrey(Vec3 A, Vec3 C, Vec3 bestSolution, Vec3 candidate)
+    {
+        //optDistance = Module(C*optimalSolution - candidate)
+        Vec3 dVector = Vec3.WolfProduct(C, bestSolution);
+        dVector.Substract(candidate);
+        double optDistance = dVector.Module;
+
+        //Calculate XVector=optimalSolution - optDistance*A
+        A.Multiply(optDistance);
+        Vec3 newSolution = Vec3.Substract(bestSolution, A);
+        return newSolution;
+    }
+
+    private Vec3 ExplorationPhase(Vec3 A, Vec3 C, Vec3 randomSolution, Vec3 candidate)
+    {
+        //optDistance = Module(C*optimalSolution - candidate)
+        Vec3 dVector = Vec3.WolfProduct(C, randomSolution);
+        dVector.Substract(candidate);
+        double optDistance = dVector.Module;
+
+        //Calculate XVector=optimalSolution - optDistance*A
+        A.Multiply(optDistance);
+        Vec3 newSolution = Vec3.Substract(randomSolution, A);
+        return newSolution;
+    }
+
+    private Vec3 SpiralUpdate(double b, double l, Vec3 bestSolution, Vec3 candidate)
+    {
+        //optDistance = Module(C*optimalSolution - candidate)
+        Vec3 dVector = Vec3.Substract(bestSolution, candidate);
+        double optDistance = dVector.Module;
+        Vec3 newSolution = new Vec3(optDistance, 0, optDistance);
+        newSolution.Multiply(Math.Exp(b) * Math.Cos(2*Math.PI*l));
+        newSolution.Add(bestSolution);
+
+        return newSolution;
     }
 
     public override int GetFixedPreyId(Animal agent)
