d4061cd1512ce580ac2cb6eb667f8b3867a6de16
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2021-09-13 10:18:07 +0200

    Add candidate solution class and a few other methods

--- EcoSim/Assets/Model/Hunting/PSOStrategy.cs
+++ EcoSim/Assets/Model/Hunting/PSOStrategy.cs
@@ -3,6 +3,29 @@ using System.Collections.Generic;
 
 public class PSOStrategy : HuntingStrategy, MetaHeuristic
 {
+    private int METAHEURISTIC_ITERATIONS = 200;
+    private int METAHEURISTIC_CANDIDATES = 10; //MUST BE >= 3
+    private int SIZE_OF_SPACE = 30;
+
+    private class CandidateSolution
+    {
+        private Vec3 _solution;
+        private Vec3 _velocity;
+        private Vec3 _personalBest;
+        public Vec3 Solution { get => _solution; set => _solution = value; }
+        public Vec3 Velocity { get => _velocity; set => _velocity = value; }
+        public Vec3 PersonalBest { get => _personalBest; set => _personalBest = value; }
+
+        private double _fitness;
+        public double Fitness { get => _fitness; set => _fitness = value; }
+
+        public CandidateSolution(Vec3 solution, double fitness)
+        {
+            this.Solution = solution;
+            this.Fitness = fitness;
+        }
+    }
+
     public override Vec3 GetDesiredPosition(Animal agent, List<Vec3> friendlyPositions, Animal prey)
     {
         throw new System.NotImplementedException();
@@ -49,6 +72,8 @@ public class PSOStrategy : HuntingStrategy, MetaHeuristic
 
     public double CalculateFitness(Vec3 candidateSolution, List<Vec3> predatorPositions, Animal prey)
     {
-        throw new System.NotImplementedException();
+        Vec3 predictedPreyPosition = this.PredictPreyPosition(candidateSolution, prey.Position, prey.MaxSquaredSpeed, prey.VisionRadius);
+        double fitness = ObjectiveFunction(predatorPositions, predictedPreyPosition, candidateSolution);
+        return fitness;
     }
 }
